e9c9a6a2fb27c3c805a64b998bbf0fb184e1530b
Navid Salehi <nvdsalehi@gmail.com>
2021-11-30 19:26:52 +0330

    add typescript and esbuild

--- src/darkmode.ts
+++ src/darkmode.ts
@@ -1,16 +1,18 @@
 const darkmode = () => {
     const darkmodeButton = document.getElementById('darkmode-btn');
     const body = document.getElementsByTagName('body')[0];
-    if(darkmodeButton.dataset['dark'] === 'false') {
+    if(darkmodeButton && darkmodeButton.dataset['dark'] === 'false') {
         body.style.backgroundColor = '#404040';
         body.style.color = 'white';
         darkmodeButton.dataset['dark'] = 'true';
         darkmodeButton.innerText = 'Light Mode';
     }
-    else if(darkmodeButton.dataset['dark'] === 'true') {
+    else if(darkmodeButton && darkmodeButton.dataset['dark'] === 'true') {
         body.style.backgroundColor = 'white';
         body.style.color = 'black';
         darkmodeButton.dataset['dark'] = 'false';
         darkmodeButton.innerText = 'Dark Mode';
     }
 }
+
+export default darkmode;
---
+++ src/window.d.ts
@@ -0,0 +1,5 @@
+declare global {
+    interface Window { darkmode: any; }
+}
+
+window.darkmode = window.darkmode || {};
